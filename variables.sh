54d795ae17f6ae832c5a116676c2cc71d348440d
unknown <vikas.arisela@gmail.com>
2026-09-20 17:52:08 +0400

    shell

--- variables.sh
+++ variables.sh
@@ -28,9 +28,9 @@ echo "${fruits[@]}"
 
 echo "provide number"
 read number
-if [ $number -lt 10 ]; then 
+if [ "$number" -lt 10 ]; then 
    echo "give number $number is less than 10"
-elif [ $number -eq 10 ]; then 
+elif [ "$number" -eq 10 ]; then 
    echo "give number $number equal to 10"
 else 
    echo "give number $number greater than 10"

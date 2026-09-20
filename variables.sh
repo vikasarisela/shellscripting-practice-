b383f4bd414cd175d5b5af5bb7ee3b2f1c48b540
unknown <vikas.arisela@gmail.com>
2026-09-20 16:53:44 +0400

    shell

--- variables.sh
+++ variables.sh
@@ -6,3 +6,10 @@ Person2="$2"
 echo "hi i am $Person"
 echo "printing $Person1 and $Person2"
 
+output=$(date)
+
+starttime=$(date, %s)
+sleep 10 
+endtime=$(date, %s)
+total_time=$(($endtime - $starttime))
+echo "print $total_time"
